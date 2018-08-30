7ee3b1c51c45dcc3fbecdaa17afe984b1d3b0f41
Jamie <960492612@qq.com>
2018-08-30 16:00:28 +0800

    易仓api

---
+++ src/components/Index/Yicang/Beihuo/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <div class="header">
+            <el-button type="primary">更新库存信息</el-button>
+            <el-button type="info">更新仓库信息</el-button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
